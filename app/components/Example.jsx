acb1b93dd0a703652545dafa5606ededec0aceb7
Harish <harish@infeedo.com>
2018-03-11 17:21:04 +0530

    adding styles

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,8 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Example = (props) => {
 	return (
-			<h2>Example Page</h2>
+		<div>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are few example locatins to try out:</p>
+			<ol>
+				<li>
+					<Link to="/?location=Gurgaon">Gurgaon</Link>
+				</li>
+				<li>
+					<Link to="/?location=Goa">Goa</Link>
+				</li>	
+				<li>
+					<Link to="/?location=Rio">Rio, Brazil</Link>
+				</li>	
+			</ol>
+			</div>
 		)
 }
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -42,12 +42,5 @@ var Nav = React.createClass ({
 	}
 });
 
- // <div>
- //                <h2>Nav Component</h2> 
- //                <IndexLink to='/' activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>Get Weather</IndexLink>
- //                <Link to='about' activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>About</Link>
- //                <Link to='example' activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>Example</Link>
- //            </div>   
-
 
 module.exports = Nav; 
